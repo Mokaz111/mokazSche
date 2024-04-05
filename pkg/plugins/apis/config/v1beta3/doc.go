b3dda358717f39af57a85c23156062a90eac2f05
mokaz111 <mokaz111@163.com>
2024-04-05 10:23:22 -0700

    feat:generated the code

--- pkg/plugins/apis/config/v1beta3/doc.go
+++ pkg/plugins/apis/config/v1beta3/doc.go
@@ -1,9 +1,9 @@
 // +k8s:deepcopy-gen=package,register
-// +k8s:conversion-gen=github.com/gocrane/crane-scheduler/pkg/plugins/apis/config
+// +k8s:conversion-gen=github.com/mokaz111/mokazSche/pkg/plugins/apis/config
 // +k8s:defaulter-gen=TypeMeta
-// +k8s:defaulter-gen-input=github.com/gocrane/crane-scheduler/pkg/plugins/apis/config/v1beta3
+// +k8s:defaulter-gen-input=github.com/mokaz111/mokazSche/pkg/plugins/apis/config/v1beta3
 
 // +groupName=kubescheduler.config.k8s.io
 
 // Package v1beta3 is the v1beta3 version of the API.
-package v1beta3 // import "crane.io/scheduler-plugins/apis/config/v1beta3"
+package v1beta3 // import "mokaz111.io/scheduler-plugins/apis/config/v1beta3"
